3a657b7fb6d84bcf6b8ab23d8ab15318c422726a
Derek T. Jones <dtj@google.com>
2018-12-10 16:43:26 +0000

    Avoid buffer edge conditions in sharding
    Do `ds.take()` up front, before sharding, default the interleave
    buffer, and use a larger shuffle.
    
    Testing shows that leaving the block size and buffer output size
    defaulted works well in this case, meaning that there is room to
    increase the final shuffle buffer to 16k.

--- bigtable_input.py
+++ bigtable_input.py
@@ -294,23 +294,22 @@ class GameQueue:
         # of the total moves, then trimming the result.
         total_moves = self.count_moves_in_game_range(start_game, end_game)
         probability = moves / (total_moves * 0.99)
-        utils.dbg('Row range: %s - %s; total moves: %d; probability %.3f' % (
-            start_row, end_row, total_moves, probability))
+        utils.dbg('Row range: %s - %s; total moves: %d; probability %.3f; moves %d' % (
+            start_row, end_row, total_moves, probability, moves))
         shards = 8
         ds = self.tf_table.parallel_scan_range(start_row, end_row,
                                                probability=probability,
-                                               num_parallel_scans=shards,
                                                columns=[(column_family, column)])
+        ds = ds.take(moves)
         if shuffle:
             rds = tf.data.Dataset.from_tensor_slices(
                 tf.random_shuffle(tf.range(0, shards, dtype=tf.int64)))
             ds = rds.apply(
                 tf.contrib.data.parallel_interleave(
                     lambda x: ds.shard(shards, x),
-                    cycle_length=shards, block_length=1024,
+                    cycle_length=shards,
                     sloppy=True))
-            ds = ds.shuffle(shards * 1024 * 2)
-        ds = ds.take(moves)
+            ds = ds.shuffle(shards * 1024 * 16)
         return ds
 
     def moves_from_last_n_games(self, n, moves, shuffle,
